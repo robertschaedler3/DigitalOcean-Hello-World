6ca52424ff86039e035d7f896e91bd866ed188f1
Robert Schaedler III <robertschaedler3@gmail.com>
2020-02-11 12:47:48 -0500

    create base route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello DigitalOcean!'))
+app.get('/', (req, res) => res.send('Hello DigitalOcean!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`));
